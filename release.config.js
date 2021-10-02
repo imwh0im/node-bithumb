7a2bbe85965f4dd22d18d3035a637ed7ef67b1a4
henry <48340699+imwh0im@users.noreply.github.com>
2021-10-03 00:46:17 +0900

    ci: change semantic release config

---
+++ release.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  branches: ['main', 'next'],
+  'plugin': [
+    '@semantic-release/npm',
+    [
+      '@semantic-releaes/github', {
+        'successComment': ':tada: This ${issue.pull_request ? "PR is included" : "issue has been resolved"} in'
+        + '[version ${nextRelease.version}](${releases.filter(release => /github.com/i.test(release.url))[0].url}) :tada:'
+      }
+    ]
+  ]
+}
