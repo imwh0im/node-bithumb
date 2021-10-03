33205e2338bd10d666b31a1616a17922e570846c
henry <48340699+imwh0im@users.noreply.github.com>
2021-10-03 16:58:41 +0900

    fix: change post balance method return type (#13)
    * fix: union literal type field
    
    * fix: post balance return type

--- types/balance.d.ts
+++ types/balance.d.ts
@@ -1,5 +1,14 @@
 import { IBithumbResponse } from './bithumb-response';
 
+interface IBalance {
+  total_coin: string
+  total_krw: string;
+  in_use_coin: string;
+  in_use_krw: string;
+  available_coin: string;
+  available_krw: string
+  xcoin_last_coin: string;
+}
 export interface IPostBalance extends IBithumbResponse {
-  data: unknown;
+  data: IBalance;
 }
