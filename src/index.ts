8505f8d60fd35cbb6264049d998629e3a9e4cba2
henry <48340699+imwh0im@users.noreply.github.com>
2021-10-20 15:42:56 +0900

    fix: header authorization

--- src/index.ts
+++ src/index.ts
@@ -339,8 +339,14 @@ export default class ApiBithumb {
   }
 
   private getBithumbHeaders(endpoint: string, parameters = {}) {
+    const parameter = {
+      parameters,
+      ...{
+        endpoint,
+      },
+    };
     const nonce = new Date().getTime();
-    const requestSignature = `${endpoint}${String.fromCharCode(0)}${quertString.stringify(parameters)}${String.fromCharCode(0)}${nonce}`;
+    const requestSignature = `${endpoint}${String.fromCharCode(0)}${quertString.stringify(parameter)}${String.fromCharCode(0)}${nonce}`;
     const hmacSignature = Buffer.from(crypto.createHmac('sha512', this.secretKey).update(requestSignature)
       .digest('hex')).toString('base64');
     return {
